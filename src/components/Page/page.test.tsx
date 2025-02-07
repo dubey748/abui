adcaa66203d13856241efdae9c99cdd0723f8db8
ABhishek Dubey <dubeyabhishek748@gmail.com>
2025-02-08 00:11:26 +0530

    Added syled-components

--- src/components/Page/page.test.tsx
+++ src/components/Page/page.test.tsx
@@ -15,4 +15,18 @@ describe('Page Component', () => {
     expect(titleElement).toBeInTheDocument();
     expect(childrenElement).toBeInTheDocument();
   });
+  it("renders the correct styling", () => {
+  
+    const title = "Test Title";
+    const children = "Test Children";
+    const { getByTestId } = render(<Page title={title}>{children}</Page>);
+
+
+    const container = getByTestId("page-container");
+
+
+    expect(container).toHaveStyle(`
+    background-color: #f5f5f5;
+    `);
+  });
 });
